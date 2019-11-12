e1e16736c4ebc66633b6efa4477f7dc88ada716d
Aryeh Nigri <aryehnigri@gmail.com>
2019-11-12 22:52:33 +0200

    GLFW Window and Windows Events

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,18 +1,46 @@
+#include "hzpch.h"
 #include "Application.h"
+
+#include "Hazel/Log.h"
+
+#include <GLFW/glfw3.h>
 
 namespace Hazel {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+//#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
+		//m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
+	Application::~Application()  {}
 
-	Application::~Application()
+	void Application::OnEvent(Event& e)
 	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		HZ_CORE_TRACE("{0}", e);
 	}
 
-	void Application::Run() {
-		while (true);
+	void Application::Run() 
+	{
+		while (m_Running)
+		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
