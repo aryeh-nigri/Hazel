67d4c61e513442e4e92aeff8eed0f03904e24fb6
Aryeh Nigri <aryehnigri@gmail.com>
2019-11-24 01:26:14 +0200

    Added buffer layout descriptors for vertex buffers

--- Hazel/src/Hazel/LayerStack.h
+++ Hazel/src/Hazel/LayerStack.h
@@ -18,8 +18,8 @@ namespace Hazel {
 		void PopLayer(Layer* layer);
 		void PopOverlay(Layer* overlay);
 
-		std::vector<Layer*>::iterator begin() { return m_Layers.begin(); }
-		std::vector<Layer*>::iterator end() { return m_Layers.end(); }
+		inline std::vector<Layer*>::iterator begin() { return m_Layers.begin(); }
+		inline std::vector<Layer*>::iterator end() { return m_Layers.end(); }
 	private:
 		std::vector<Layer*> m_Layers;
 		unsigned int m_LayerInsertIndex = 0;
